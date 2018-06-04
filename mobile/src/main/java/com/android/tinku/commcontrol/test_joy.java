3ad09d8c2de9df19a1bef9e66bde55b6387d0b89
SasankB <sasankbharadwaj@gmail.com>
2018-06-05 01:48:24 +0530

    stable version
    stable version

--- mobile/src/main/java/com/android/tinku/commcontrol/test_joy.java
+++ mobile/src/main/java/com/android/tinku/commcontrol/test_joy.java
@@ -1,48 +1,86 @@
 package com.android.tinku.commcontrol;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class test_joy extends AppCompatActivity {
 
-    private TextView mTextViewAngleLeft;
-    private TextView mTextViewStrengthLeft;
-
-    private TextView mTextViewAngleRight;
-    private TextView mTextViewStrengthRight;
-
+    public SharedPreferences Prefs;
+    private int angle =  0;
+    private int strength = 0;
+    private int throttlePower = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_joy);
 
+        Intent startUdpService = new Intent(this, UdpService.class);
 
-        mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
-        mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
+        Prefs = getSharedPreferences(ApplicationConstants.ApplicationPrefs, Context.MODE_PRIVATE);
+        Prefs.edit().putString(ApplicationConstants.DirectionKey,"0,0,0").commit();
+        Prefs.edit().putBoolean(ApplicationConstants.ServiceStartKey,true).commit();
+        this.startService(startUdpService);
+        final SeekBar throttle = (SeekBar) findViewById(R.id.throttle);
 
-        JoystickView joystickLeft = (JoystickView) findViewById(R.id.joystickView_left);
-        joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
+        throttle.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
             @Override
-            public void onMove(int angle, int strength) {
-                mTextViewAngleLeft.setText(angle + "°");
-                mTextViewStrengthLeft.setText(strength + "%");
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                SetData(angle, strength, throttle.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
             }
         });
 
-
-        mTextViewAngleRight = (TextView) findViewById(R.id.textView_angle_right);
-        mTextViewStrengthRight = (TextView) findViewById(R.id.textView_strength_right);
-
-        JoystickView joystickRight = (JoystickView) findViewById(R.id.joystickView_right);
+        JoystickView joystickRight = (JoystickView) findViewById(R.id.joystickView_left);
         joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
-            public void onMove(int angle, int strength) {
-                mTextViewAngleRight.setText(angle + "°");
-                mTextViewStrengthRight.setText(strength + "%");
+            public void onMove(int _angle, int _strength)
+            {
+                angle = _angle;
+                strength = _strength;
+                if ( angle == 0 && strength == 0)
+                {
+                    SetData(angle, strength, throttle.getProgress());
+                }
+                else
+                {
+                    angle = angle + 90;
+                    if (angle >= 360)
+                        angle-= 360;
+                    SetData(angle, strength, throttle.getProgress());
+                }
+
             }
         });
+    }
+
+    private  void SetData(int angle, int strength, int height)
+    {
+        String data = String.format("%s,%s,%s",angle,strength,height);
+        Intent startUdpService = new Intent(this, UdpService.class);
+        Prefs.edit().putString(ApplicationConstants.DirectionKey,data).commit();
+        Prefs.edit().putBoolean(ApplicationConstants.ServiceStartKey,true).commit();
+        if (Prefs.getBoolean(ApplicationConstants.ServiceStartKey,true))
+        {
+            startService(startUdpService);
+        }
     }
 }
